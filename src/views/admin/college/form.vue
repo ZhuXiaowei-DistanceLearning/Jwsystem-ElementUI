dfa9ceedc508f0a1843ea95bd7b265ccadf141a6
zxw <502513206@qq.com>
2019-12-26 15:32:59 +0800

    完成学院模块、登录模块

---
+++ src/views/admin/college/form.vue
@@ -0,0 +1,94 @@
+<template>
+  <el-dialog :append-to-body="true" :close-on-click-modal="false" :before-close="cancel" :visible.sync="dialog" :title="isAdd ? '新增' : '编辑'" width="500px">
+    <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
+      <el-form-item label="编号" >
+        <el-input v-model="form.id" style="width: 370px;"/>
+      </el-form-item>
+      <el-form-item label="学院名称" >
+        <el-input v-model="form.name" style="width: 370px;"/>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button type="text" @click="cancel">取消</el-button>
+      <el-button :loading="loading" type="primary" @click="doSubmit">确认</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import { add, edit } from '@/api/admin/college/college'
+export default {
+  props: {
+    isAdd: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      loading: false, dialog: false,
+      form: {
+        id: '',
+        name: '',
+      },
+      rules: {
+      }
+    }
+  },
+  methods: {
+    cancel() {
+      this.resetForm()
+    },
+    doSubmit() {
+      this.loading = true
+      if (this.isAdd) {
+        this.doAdd()
+      } else this.doEdit()
+    },
+    doAdd() {
+      add(this.form).then(res => {
+        this.resetForm()
+        this.$notify({
+          title: '添加成功',
+          type: 'success',
+          duration: 2500
+        })
+        this.loading = false
+        // this.$parent.load()
+        this.$emit('close')
+      }).catch(err => {
+        this.loading = false
+        console.log(err.response.data.message)
+      })
+    },
+    doEdit() {
+      edit(this.form).then(res => {
+        this.resetForm()
+        this.$notify({
+          title: '修改成功',
+          type: 'success',
+          duration: 2500
+        })
+        this.loading = false
+        // this.$parent.load()
+        this.$emit('close')
+      }).catch(err => {
+        this.loading = false
+        console.log(err.response.data.message)
+      })
+    },
+    resetForm() {
+      this.dialog = false
+      this.$refs['form'].resetFields()
+      this.form = {
+        id: '',
+        name: '',
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/admin/college/index.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="app-container">
+    <!--工具栏-->
+    <div class="head-container">
+      <!-- 搜索 -->
+      <el-input v-model="params.keyword" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item"
+                @keyup.enter.native="toQuery"/>
+      <el-button class="filter-item" size="mini" type="success" icon="el-icon-search" @click="toQuery">搜索</el-button>
+      <el-button class="filter-item" size="mini" type="primary" icon="el-icon-plus" @click="add">新增</el-button>
+    </div>
+    <!--表单组件-->
+    <e-from ref="form" :is-add="isAdd" @close="load()"/>
+    <!--表格渲染-->
+    <el-table
+      v-loading="loading"
+      :data="data"
+      size="small"
+      style="width: 100%;"
+      stripe
+      empty-text="暂无数据"
+      highlight-current-row
+      current-row-key="id"
+    >
+      <el-table-column prop="id" label="编号"/>
+      <el-table-column prop="name" label="名称"/>
+      <el-table-column prop="status" label="状态" show-overflow-tooltip>
+        <template slot-scope="scope">
+          <el-tag
+            :type="scope.row.status==1?'success':'danger'"
+            disable-transitions> {{scope.row.status==1?'正在使用':'已作废'}}
+          </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="200px"
+                       align="center">
+        <template slot-scope="scope">
+          <el-row>
+            <el-col :span="10">
+              <el-button size="mini" type="primary" icon="el-icon-edit" @click="edit(scope.row)">编辑
+              </el-button>
+            </el-col>
+            <el-col :span="4"></el-col>
+            <el-col :span="10">
+              <el-button size="mini" type="danger" icon="el-icon-delete"
+                         @click="subDelete(scope.row.id)">{{scope.row.status==1?'作废':'恢复'}}
+              </el-button>
+            </el-col>
+          </el-row>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!--分页组件-->
+    <el-row>
+      <el-col :offset="10">
+        <el-pagination
+          :total="total"
+          :current-page="params.limit"
+          style="margin-top: 8px;"
+          :page-sizes="[10, 20, 30, 50]"
+          layout="total, prev, pager, next, sizes"
+          @size-change="sizeChange"
+          @current-change="pageChange"/>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  import service from '../../../utils/request'
+  import eFrom from './form'
+  import { del } from '@/api/admin/college/college'
+
+  export default {
+    components: {
+      eFrom
+    },
+    data() {
+      return {
+        loading: true,
+        delLoading: false,
+        total: 0,
+        isAdd: false,
+        params: {
+          offset: 1,
+          limit: 10,
+          keyword: null,
+          status: null
+        },
+        data: null
+      }
+    },
+    created() {
+      service.get('/api/college/pageQuery', { params: this.params }).then(res => {
+        this.data = res.records
+        this.currentPage = res.current
+        this.loading = false
+        this.total = res.realTotal
+      }).catch(error => {
+        this.loading = false
+        this.$notify({
+          title: '错误信息',
+          message: '数据加载失败'
+        })
+      })
+    },
+    methods: {
+      load() {
+        service.get('/api/college/pageQuery', { params: this.params }).then(res => {
+          this.data = res.records
+          this.loading = false
+          this.currentPage = res.current
+          this.total = res.realTotal
+        }).catch(error => {
+          this.loading = false
+          this.$notify({
+            title: '错误信息',
+            message: '数据加载失败'
+          })
+        })
+      },
+      sizeChange(val) {
+        this.params.limit = val
+        this.load()
+      },
+      pageChange(val) {
+        this.params.offset = val
+        this.load()
+      },
+      subDelete(id) {
+        this.delLoading = true
+        this.$confirm('确定进行此操作吗？', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          del(id).then(res => {
+            this.delLoading = false
+            this.$notify({
+              title: '成功',
+              type: 'success',
+              duration: 2500
+            })
+            this.load()
+          }).catch(err => {
+            this.delLoading = false
+            console.log(err.response.data.message)
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消'
+          })
+        })
+      },
+      add() {
+        this.isAdd = true
+        this.$refs.form.dialog = true
+      },
+      edit(data) {
+        this.isAdd = false
+        const _this = this.$refs.form
+        _this.form = {
+          id: data.id,
+          name: data.name
+        }
+        _this.dialog = true
+      },
+      toQuery() {
+        this.load()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .status {
+    color: green;
+  }
+</style>
